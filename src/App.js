d6a794632e180d15d6d8440024a7b347b297421d
okada001 <okada001@rangers.uwp.edu>
2019-06-26 22:47:36 +0900

    added useState for search bar and new css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
 import Recipe from "./Recipe";
+import { SSL_OP_NETSCAPE_REUSE_CIPHER_CHANGE_BUG } from 'constants';
 require('dotenv').config()
 
 
@@ -12,32 +13,44 @@ const App = () => {
   const APP_KEY = process.env.REACT_APP_KEY;
 
   const [recipes,setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] =useState("chicken");
   
 
   //useEffect will run everytime the website renders a new change but it's should be avoided
   //putting empty array by [] works in here in order not to send billions of request by each render.
   useEffect(() =>{
     getRecipes();
-  },[]);
+  },[query]);
 
   const getRecipes = async () => {
     //Do await in order to wait till information comes back cuz the info comes from other place
     //it takes time you know
-    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
     setRecipes(data.hits);
     console.log(data.hits);
   };
 
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  };
+
+  const getSearch = e =>{
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+  }
+
   return (
     <div className="App">
-      <h1>Hello React</h1>
-      <form className="search-form">
-        <input className ="search-bar" type="text"/>
+      <form onSubmit ={getSearch} className="search-form">
+        <input className ="search-bar" type="text" value ={search} onChange={updateSearch}/>
         <button className ="search-button" type="submit"> 
         Search
         </button>
       </form>
+      <div className="recipes">
 
       {recipes.map(recipe => (
         <Recipe 
@@ -45,8 +58,10 @@ const App = () => {
         title={recipe.recipe.label} 
         calories ={recipe.recipe.calories}
         image ={recipe.recipe.image}
+        ingredients ={recipe.recipe.ingredients}
         />
       ))}
+      </div>
     </div>
   );
 }
